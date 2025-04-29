b108de4ca5a263d010373f0b8c82e2a092f6fd30
John Thompson <john.thompson@affinity-digital.com>
2025-04-29 16:48:19 +0100

    chore: patch rogue styles

--- wp-content/themes/whalepress/postcss.config.js
+++ wp-content/themes/whalepress/postcss.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   plugins: {
-    // any PostCSS plugins you need
-    'postcss-preset-env': {},
-
-    // ← this one MUST be here
     '@tailwindcss/postcss': {},
-
-    // keep autoprefixer if you like
-    'autoprefixer': {},
   },
 }
--- wp-content/themes/whalepress/tailwind.config.js
+++ wp-content/themes/whalepress/tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: 'jit',
-	important: true,
 	content: [
 		'./*/*.{twig,php}',
 		'./**/*.{twig,php}',
-		'./public/src/js/*.js',
+		//'./public/src/js/*.js',
 		'./safelist.txt',
 		'!./node_modules/**',
 		'!./vendor/**',
